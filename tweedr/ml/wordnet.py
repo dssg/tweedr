257b7c1b419c8b76884bfbc4147d5a9956e4b123
Manojit Nandi <mnandi92@gmail.com>
2013-07-29 11:16:43 -0500

    Added wordnet to ml folder

---
+++ tweedr/ml/wordnet.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+from pattern.en import wordnet
+import numpy as np
+
+
+def WordnetFeatures(token):
+    synset = wordnet.synsets(token)
+    if len(synset) > 0:
+        synset = synset[0]
+        hypernym = synset.hypernyms(depth=2, recursive=True)
+#       hypernym.extend(synset.hyponyms(depth=2,recursive=True))
+        return [hyper.senses[0] for hyper in hypernym]
+    else:
+        return []
+
+
+def WordNet(document):
+    return [WordnetFeatures(token) for token in document]

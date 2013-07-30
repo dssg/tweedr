ef4180299e30e6bbaf528e9b9535936d8ce6cadf
Christopher Brown <io@henrian.com>
2013-07-30 10:00:47 -0500

    Only give Travis CI a pass on the credentials test

--- tests/credentials.py
+++ tests/credentials.py
@@ -6,17 +6,18 @@ import tweedr
 
 class TestCredentials(unittest.TestCase):
     def test_mysql(self):
-        '''For obvious reasons, Travis CI cannot run this test. Simply pad out
-        the test with dummy values when the variables aren't available.'''
-        names = ['MYSQL_PASS', 'MYSQL_HOST']
-        values = [os.environ.get(name, 'MYSQL_DUMMY_CREDENTIAL') for name in names]
+        if 'TRAVIS' in os.environ:
+            print 'For obvious reasons, Travis CI cannot run this test.'
+        else:
+            names = ['MYSQL_PASS', 'MYSQL_HOST']
+            values = [os.environ[name] for name in names]
 
-        for base, directories, filenames in os.walk(tweedr.root):
-            for filename in filenames:
-                filepath = os.path.join(base, filename)
-                with open(filepath) as fp:
-                    contents = fp.read()
-                    for value in values:
-                        # assertNotIn(first, second, msg=None)
-                        #   Test that first is (or is not) in second.
-                        self.assertNotIn(value, contents, 'Found a blacklisted credential (%s) in %s' % (value, filepath))
+            for base, directories, filenames in os.walk(tweedr.root):
+                for filename in filenames:
+                    filepath = os.path.join(base, filename)
+                    with open(filepath) as fp:
+                        contents = fp.read()
+                        for value in values:
+                            # assertNotIn(first, second, msg=None)
+                            #   Test that first is (or is not) in second.
+                            self.assertNotIn(value, contents, 'Found a blacklisted credential (%s) in %s' % (value, filepath))

3994e8740d5d2c7638aafd8ad12ceb77f400eb42
Christopher Brown <io@henrian.com>
2013-08-19 15:41:32 -0500

    Change SequenceTagger to pull out sequences rather than just give list of labels

--- tweedr/api/mappers/nlp.py
+++ tweedr/api/mappers/nlp.py
@@ -1,10 +1,11 @@
 from tweedr.api.mappers import Mapper
 from tweedr.api.protocols import TweetDictProtocol
-from tweedr.lib.text import token_re
+from tweedr.lib import mapList
+from tweedr.lib.text import token_re, zip_boundaries
 from tweedr.ml.ark import TwitterNLP
 from tweedr.ml.crf.classifier import CRF
 from tweedr.ml.features import crf_feature_functions, featurize
-
+import itertools
 
 import logging
 logger = logging.getLogger(__name__)
@@ -44,12 +45,28 @@ class SequenceTagger(Mapper):
         logger.info('SequenceTagger initialized')
 
     def __call__(self, tweet):
-        text = tweet['text'].encode('utf8')
+        text = tweet['text']
         tokens = token_re.findall(text)
 
         # tokens_features = map(list, featurize(tokens, crf_feature_functions))
         tokens_features = featurize(tokens, crf_feature_functions)
+        # print 'tokens_features', mapList(tokens_features)
+
+        null_label = 'None'
         labels = self.crf.predict([tokens_features])[0]
-        tweet['labels'] = labels
+        # tweet['labels'] = labels
+
+        if 'sequences' not in tweet:
+            tweet['sequences'] = []
+
+        for sequence_label, entries in itertools.groupby(zip_boundaries(labels), lambda tup: tup[0]):
+            if sequence_label != null_label:
+                labels, starts, ends = zip(*entries)
+
+                tweet['sequences'].append({
+                    'text': sequence_label,
+                    'start': starts[0],
+                    'end': ends[-1],
+                })
 
         return tweet

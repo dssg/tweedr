64fcf99353270f765e41a89b771d7cdf78b4a20b
Christopher Brown <io@henrian.com>
2013-08-22 11:47:03 -0500

    Add a few more spreadsheets and counts to QCRI corpora

--- tweedr/corpora/qcri.py
+++ tweedr/corpora/qcri.py
@@ -18,6 +18,7 @@ class CSVDatasouce(DatasourceI):
 
 
 class a131709_datasource(CSVDatasouce):
+    # from joplin/
     '''
     Counts:
 
@@ -33,6 +34,7 @@ class a131709_datasource(CSVDatasouce):
 
 
 class a121571_datasource(CSVDatasouce):
+    # from joplin/
     '''
     Counts:
 
@@ -50,6 +52,7 @@ class a121571_datasource(CSVDatasouce):
 
 
 class a122047_datasource(CSVDatasouce):
+    # from joplin/
     '''
     Counts:
 
@@ -65,6 +68,7 @@ class a122047_datasource(CSVDatasouce):
 
 
 class a126730_datasource(CSVDatasouce):
+    # from joplin/
     '''
     Counts:
 
@@ -82,6 +86,7 @@ class a126730_datasource(CSVDatasouce):
 
 
 class a126728_datasource(CSVDatasouce):
+    # from joplin/
     '''
     Counts:
 
@@ -97,6 +102,88 @@ class a126728_datasource(CSVDatasouce):
     filepath = globfirst('**/a126728.csv', root=corpora_root)
     label_column = 'type_of_donation'
     text_column = 'text'
+
+
+class a122582_datasource(CSVDatasouce):
+    # from joplin/
+    '''
+    Counts:
+
+      4 Tune to this radio station (or: I am listening to this station)
+     10 Watch this TV channel (or: I am watching this channel)
+     33 None of the above
+     35 Look at this photo or these photos
+     58 Look at this video or these videos
+    139 Look at this web site/page
+    '''
+    filepath = globfirst('**/a122582.csv', root=corpora_root)
+    label_column = 'type_of_message'
+    text_column = 'text'
+
+
+class a143145_datasource(CSVDatasouce):
+    # from sandy/
+    '''
+    Counts:
+
+     78 Informative (Direct)
+     79 Informative (Direct or Indirect)
+    161 Other
+    296 Personal Only
+    386 Informative (Indirect)
+    '''
+    filepath = globfirst('**/a143145.csv', root=corpora_root)
+    label_column = 'choose_one'
+    text_column = 'tweet'
+
+
+class a144267_datasource(CSVDatasouce):
+    # from sandy/
+    '''
+    Counts:
+
+     32 Donations of money, goods or services
+     72 Information Source
+    125 Unknown
+    144 Caution and advice
+    170 Casualties and damage
+    '''
+    filepath = globfirst('**/a144267.csv', root=corpora_root)
+    label_column = 'choose_one'
+    text_column = 'tweet'
+
+
+class a146283_datasource(CSVDatasouce):
+    # from sandy/
+    '''
+    Counts:
+
+     6 A shelter is open or available
+    20 A hurricane warning has been issued or has been lifted
+    23 A hurricane sighting has been reported
+    77 Other
+    '''
+    filepath = globfirst('**/a146283.csv', root=corpora_root)
+    label_column = 'type_of_advice_or_caution'
+    text_column = 'tweet'
+
+
+class a146281_datasource(CSVDatasouce):
+    # from sandy/
+    '''
+    Counts:
+
+     1 People: injured
+     3 People: injured and dead
+    12 Not specified (maybe people or infrastructure)
+    13 Both people and infrastructure
+    16 Not damage-related
+    34 People: dead
+    91 Infrastructure (building, bridge, road, etc.) damage
+    '''
+    filepath = globfirst('**/a146281.csv', root=corpora_root)
+    label_column = 'people_or_infrastructure'
+    text_column = 'tweet'
 
 
 if __name__ == '__main__':
